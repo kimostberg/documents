a427642c6e7ab9cbce3dc31b2b499a1925926c8a
KimOstberg <mail@kimostberg.it>
2024-05-29 17:32:26 +0300

    Update install.ps1

--- scripts/netbird/client/install.ps1
+++ scripts/netbird/client/install.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri "https://pkgs.netbird.io/windows/x64" -OutFile "C:\Temp\NetBird-Setup.exe"
 Start-Process -FilePath "C:\Temp\NetBird-Setup.exe" -ArgumentList "/S" -Wait
-Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"` | Where-Object {$_.DisplayName -like "*NetBird*"}
 Start-Process -FilePath "C:\Program Files\NetBird\NetBird.exe" -ArgumentList "login --management-url https://netbird.ktj.solutions:33073 --admin-url https://netbird.ktj.solutions"
+Start-Process -FilePath "C:\Program Files\NetBird\NetBird.exe" -ArgumentList "up"
